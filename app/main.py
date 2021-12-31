e2e2915980348b71aebfa3ac2cd6c777ffbe3256
Malay Damani <damanimalay@gmail.com>
2021-12-31 17:19:21 +0530

    done

---
+++ app/main.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,url_for
+import pickle
+app=Flask(__name__)
+
+@app.route('/')
+def Home():
+    return render_template('index.html')
+
+@app.route("/user/<name>")
+def user(name):
+    return f"My name is {name}"
+
+@app.route("/users_get",methods=["GET"])
+def users_get():
+    return { "id": 1,"name": "John Doe", "email": "john12@gmail.com"}
+
+@app.route("/result",methods=["POST"])
+def success():
+    user={}
+    age=request.form['age']
+    li=['age', 'workclass', 'education', 'marital-status', 'occupation',
+       'relationship', 'race', 'gender', 'capital-gain', 'capital-loss',
+       'hours-per-week', 'native-country']
+    query=[]
+    for i in li:
+        exec(f"query.append(int(request.form['{i}']))")
+    with open('pickle_model','rb') as f:
+        model=pickle.load(f)
+    result= model.predict([query])[0]
+    print(query)
+    print(model)
+    print(result)
+    if result==0:
+        return "Income is <=50K"
+    elif result==1:
+        return "Income is >50K"
+    else:
+        return "Some is wrong"
+
+with app.test_request_context():
+    url_for('Home')
+    url_for('user',name='Malay')
